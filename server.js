a0cf754d72284fd6c648f9306e40b14184671347
girish-gabra <girish.gabra10@gmail.com>
2017-06-07 18:25:42 -0700

    Added code to get names of files

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var fs = require('fs');
+var cors = require('cors');
+
 var hostname = 'localhost';
 var port = 7777;
 
 var app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 
 var Storage = multer.diskStorage({
 	destination: function(req,file,callback){
@@ -22,8 +24,10 @@ var Storage = multer.diskStorage({
 				return;
 			}	
 			// increment the number for the next file
+			console.log()
 			fileNumber=files.length+1;
 			var originalName = file.originalname;
+			console.log(originalName);
 			var index = originalName.lastIndexOf(".");
 			var changedName =  fileNumber+originalName.substr(index);
 			callback(null,changedName);
@@ -39,7 +43,23 @@ var upload = multer({
 app.get("/", function(req, res) {
      res.sendFile(__dirname + "/public/index.html");
  });
+
+app.get("/getFileNames",function(req,res){
+	//console.log("Get file names called");
+	fs.readdir(__dirname+"/Images",function(err,files){
+		if(files.length==0){
+			res.send("No Files");
+		}
+		else{
+			//console.log(files);
+			var fileStr = files.toString();
+			res.send(JSON.stringify(fileStr));
+		}
+	})
+});
+
 app.post("/uploadPost", function(req, res) {
+	console.log('uploadPost called');
      upload(req, res, function(err) {
          if (err) {
          	 console.log("error in uploading file"+err);
@@ -49,10 +69,16 @@ app.post("/uploadPost", function(req, res) {
      });
  });
 
+/*app.post("/uploadTest",function(req,res){
+	console.log('uploadTest called');
+	return res.end("test success");
+});*/
 
 app.get("/getPost/:fileName",function(req,res){
 	
-	var path = __dirname+"/Images/"+req.params.fileName;
+	var filename = req.params.fileName;
+	console.log(filename);
+	var path = __dirname+"/Images/"+filename;
 	if(fs.existsSync(path))	// check if file exists
 	{
 		res.sendFile(path);	
